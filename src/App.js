a0adc1faaed0b066f01c312c76b340b5110ba442
adish55 <adish55@gmail.com>
2018-08-21 23:27:36 +0530

    required changes made to App.js

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+
+class App extends Component {
+  /*constructor(props) {
+    super(props);
+
+    this.state = { manager: ''};
+  }*/
+
+  state = {
+    manager: '',
+    players: [],
+    balance: ''
+  };
+
+  async componentDidMount() {
+// this method is automatically invoked once the component is rendered
+    const manager = await lottery.methods.manager().call(); //from property is not required
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager, players, balance });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
